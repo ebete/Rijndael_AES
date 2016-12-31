e575567a81810b65bdcb8c294e5b6e09e836a60b
Thom Griffioen <thomgriffioen@gmail.com>
2016-12-31 03:35:18 +0100

    Implemented argparse as the program argument handler

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import sys
 import base64
+import argparse
 from Rijndael import Rijndael
 
 
 # Main entry point.
-def main(argv):
-    ao = 0
-    if len(argv) > 3:
-        plaintext = str(argv[0])
-        ao = 1
-    else:
-        plaintext = ""
-        for line in sys.stdin:
-            plaintext += line
-    secret = str(argv[0+ao])
-    statesize = int(argv[1+ao])
-    keysize = int(argv[2+ao])
+def main():
+    parser = argparse.ArgumentParser(description="Rijndael cipher implementation in Python.")
+    parser.add_argument("-s", "--state-size", metavar="bits", help="The state size to use in bits", action="store", type=int, default=128)
+    parser.add_argument("-k", "--key-size", metavar="bits", help="The key size to use in bits", action="store", type=int, default=128)
+    parser.add_argument("-p", "--passkey", metavar="key", help="The secret key to use", action="store", type=str, required=True)
+    parser.add_argument("-i", "--input", metavar="input", help="The input text to process", action="store", type=str, default=sys.stdin)
+    args = parser.parse_args()
+
+    plaintext = args.input
+    secret = args.passkey
+    statesize = args.state_size
+    keysize = args.key_size
 
     cryptoprovider = Rijndael(statesize, keysize)
     cryptoprovider.createdatablocks(plaintext)
@@ -36,4 +37,4 @@ def main(argv):
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
